165dad96226c5b7e82fa4627d539d202c5d05d8e
Muhammad Imam Al Amin <imamalamin485@gmail.com>
2026-05-05 16:07:40 +0700

    feat: add route web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\FrontPendaftaranController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('beranda');
 });
+
+Route::get('/tentang-kami', function () {
+    return view('tentang-kami');
+});
+
+Route::get('/pendaftaran', [FrontPendaftaranController::class, 'index'])->name('pendaftaran.index');
+Route::post('/pendaftaran', [FrontPendaftaranController::class, 'store'])->name('pendaftaran.store');
