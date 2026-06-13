09a102ac0dafa29743974eb19b45e0918fc6c6c2
Muhammad Imam Al Amin <imamalamin485@gmail.com>
2026-06-13 11:39:47 +0700

    feat: add guru relationship to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function guru()
+    {
+        return $this->hasOne(Guru::class, 'user_id');
+    }
 }
