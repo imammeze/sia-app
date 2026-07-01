6f7b2dde9aa5d3b5b4ebfc4b1d272a10f9f58877
Muhammad Imam Al Amin <imamalamin485@gmail.com>
2026-07-01 23:44:40 +0700

    feat: create DataPeriodik model and migration for data_periodiks table

---
+++ database/migrations/2026_04_12_150114_create_data_periodiks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('data_periodiks', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->integer('tinggi_badan')->nullable();
+            $table->integer('berat_badan')->nullable();
+            $table->integer('jumlah_saudara_kandung')->nullable();
+            $table->string('jarak_ke_sekolah')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('data_periodiks');
+    }
+};
